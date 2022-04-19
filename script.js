bec9846fcc84179ca433d8e890282ba7dc75d23a
Amadeusz Smok <amadeusz17@gmail.com>
2022-04-19 23:27:04 +0000

    removed form tag and interest integer conversion

--- script.js
+++ script.js
@@ -16,9 +16,10 @@ function compute()
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
     var interest = principal * years * rate /100;
-    var amount = parseInt(interest);
+    var amount = interest;
     var year = new Date().getFullYear() + parseInt(years);
     document.getElementById("result").innerHTML = "If you deposit " + highlightText(principal) + ",\<br\>at an interest rate of " + highlightText(rate) + "%\<br\>You will receive an amount of " + highlightText(amount) + ",\<br\>in the year "+ highlightText(year) +"\<br\>"
+    //document.getElementById("result").innerHTML = "test"
 }
 //visualises the rate slider for easier use
 function updateRate() 
